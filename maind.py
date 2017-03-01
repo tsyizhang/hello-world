ca7587294da6f17bf8559cb4101999cbfe083ca5
Zhang Yi <tsyizhang@gmail.com>
2017-03-01 21:22:27 +0800

    Create maind.py

---
+++ maind.py
@@ -0,0 +1,8 @@
+# python27
+# _*_coding:utf-8
+import numpy as np
+import pandas as pd
+import numpy.random as npr
+import matplotlib.pyplot as plt
+# cols=['AA','BB','CC','DD','EE']
+date_r=pd.date_range('1/2/2017',periods=100,freq='D')
