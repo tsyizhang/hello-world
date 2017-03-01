d6a89ec87fa8c993de9d04635f8797ee520e2aff
Zhang Yi <tsyizhang@gmail.com>
2017-03-01 21:26:52 +0800

    Create dfs.py
    "this is an example for python class only for test

---
+++ dfs.py
@@ -0,0 +1,15 @@
+class dsf():
+  import sys
+  import re
+  import os
+  def __init__(self):
+    self.name='zhan'
+    self.age=23
+    self.clss=2
+  def mifo(self,name='fg',age=23,clss=1):
+    self.name=name
+    self.age=age
+    self.clss=clss
+if __name__=='__main__':
+   print 'this is a test'
+   
